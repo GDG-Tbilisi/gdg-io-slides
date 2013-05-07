71308da516c84e49bab29781be51cb7a2ead0913
Nick Shvelidze <shveloo@gmail.com>
2013-05-07 21:35:34 +0400

    GDG 2013

--- slide_config.js
+++ slide_config.js
@@ -1,33 +1,33 @@
 var SLIDE_CONFIG = {
   // Slide settings
   settings: {
-    title: 'Title Goes Here<br>Up To Two Lines',
+    title: 'My GDG Talk',
     subtitle: 'Subtitle Goes Here',
-    //eventInfo: {
-    //  title: 'Google I/O',
-    //  date: '6/x/2013'
-    //},
+    eventInfo: {
+      title: 'Google I/O Extended',
+      date: '6/15/2013'
+    },
     useBuilds: true, // Default: true. False will turn off slide animation builds.
     usePrettify: true, // Default: true
     enableSlideAreas: true, // Default: true. False turns off the click areas on either slide of the slides.
     enableTouch: true, // Default: true. If touch support should enabled. Note: the device must support touch.
     //analytics: 'UA-XXXXXXXX-1', // TODO: Using this breaks GA for some reason (probably requirejs). Update your tracking code in template.html instead.
-    favIcon: 'images/google_developers_logo_tiny.png',
+    favIcon: 'images/gdg_logo_tiny.png',
     fonts: [
       'Open Sans:regular,semibold,italic,italicsemibold',
       'Source Code Pro'
     ],
-    //theme: ['mytheme'], // Add your own custom themes or styles in /theme/css. Leave off the .css extension.
+    theme: ['io2013-gdg'], // Add your own custom themes or styles in /theme/css. Leave off the .css extension.
   },
 
   // Author information
   presenters: [{
-    name: 'Firstname Lastname',
-    company: 'Job Title<br>Google',
-    gplus: 'http://plus.google.com/1234567890',
-    twitter: '@yourhandle',
-    www: 'http://www.you.com',
-    github: 'http://github.com/you'
+    name: 'Nikoloz Shvelidze',
+    company: 'Web Developer<br>LemonDo',
+    gplus: 'http://nicksh.com/plus',
+    twitter: '@shvelo96',
+    www: 'http://www.nicksh.com',
+    github: 'http://github.com/shvelo'
   }/*, {
     name: 'Second Name',
     company: 'Job Title, Google',
