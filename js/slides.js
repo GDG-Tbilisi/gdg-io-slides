ff5af8abdc3d0be0eb764c8e83ed40a3c99b3b5b
Nick Shvelidze <shveloo@gmail.com>
2013-05-08 17:44:44 +0400

    Add Charts

--- js/slides.js
+++ js/slides.js
@@ -1,5 +1,17 @@
-require(['order!../slide_config', 'order!modernizr.custom.45394',
-         'order!prettify/prettify', 'order!hammer', 'order!slide-controller',
-         'order!slide-deck'], function(someModule) {
+LazyLoad.js(['slide_config.js', 'js/jquery-1.9.1.min.js', 'js/modernizr.custom.45394.js',
+'js/prettify/prettify.js', 'js/hammer.js', 'js/slide-controller.js', 'js/slide-deck.js', 'js/Chart.min.js'], function() {
+    $('[data-chart]').each(function(i, el){
+    	var ctx = el.getContext('2d')
 
+    	if(!ctx) return;
+
+    	var chart_type = $(el).data('chart')
+    	var data_url = $(el).data('data')
+
+    	var chart = new Chart(ctx)
+
+    	LazyLoad.js([data_url], function() {
+    		chart.Line(chartData)
+    	})
+    }) 
 });
